fcc1a6ba937368bee6308958dd3ef7c1bfdad59f
James FL <jlacandula@gmail.com>
2022-03-31 03:33:50 +0800

    revert main.js from LIVE

--- assets/js/main.js
+++ assets/js/main.js
@@ -151,21 +151,25 @@ jQuery(document).ready(function($) {
 		}
 	}, 1000);
 
-	//Timeline Section: targets 'HTML code' widget on page /who-we-are
-	function timelineSliderMobileNav() {
-		const timelineSlider = $('#historySlider .slick-slider');
-		$('.fdm-centuries').change(function () {
-			var postId = $(this).val();
-			console.log('postId: ' + postId);
-			var slideIndex = timelineSlider.find('[data-post-id="' + postId + '"]').data('slick-index');
-			console.log('slickGoTo: ' + slideIndex);
-			timelineSlider.slick('slickGoTo', slideIndex);
-		});
-	}
+	//Timeline Section
+	setTimeout(function() {
 
-	waitForElm('#historySlider .slick-slider').then((elm) => {
-		timelineSliderMobileNav();
-	});
+		var sliderTime = $('.timeline').find('.slick-slider');
+
+		$('.century .elementor-widget-button').click(function(e) {
+			e.preventDefault();
+			var pLink = $(this).data('slide');
+			var pIndex = sliderTime.find('[data-post-id="' + pLink + '"]').data('slick-index');
+			sliderTime.slick('slickPause').slick('slickGoTo', pIndex);
+		});
+
+		$('#century').change(function() {
+			var pLink = $(this).val();
+			var pIndex = sliderTime.find('[data-post-id="' + pLink + '"]').data('slick-index');
+			sliderTime.slick('slickPause').slick('slickGoTo', pIndex);
+		});
+
+	}, 1000);
 
 
 
@@ -627,23 +631,3 @@ jQuery(document).ready(function ($) {
 	}
 
 });
-
-function waitForElm(selector) {
-	return new Promise(resolve => {
-		if (document.querySelector(selector)) {
-			return resolve(document.querySelector(selector));
-		}
-
-		const observer = new MutationObserver(mutations => {
-			if (document.querySelector(selector)) {
-				resolve(document.querySelector(selector));
-				observer.disconnect();
-			}
-		});
-
-		observer.observe(document.body, {
-			childList: true,
-			subtree: true
-		});
-	});
-}
